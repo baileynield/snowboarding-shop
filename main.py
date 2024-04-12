13889aa1d931ed316f09479c5b848e75427dbd33
Bailey Nield <bailey.nield18@gmail.com>
2024-04-12 08:48:07 -0600

    Add CRUD opertaions

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
 import json
 
+from fastapi import FastAPI
+
+from models import Board, Brand
+
+
+app = FastAPI()
 
 with open("snowboards.json", "r") as f:
     snowboard_list = json.load(f)
+
+boards: list[Board] = []
+
+for board in snowboard_list:
+    boards.append(Board(**board))
+
+
+@app.get("/boards")
+async def get_boards() -> list[Board]:
+    return boards
+
+@app.post("/boards")
+async def create_boards(board: Board) -> None:
+    boards.append(board)
+
+@app.put("/boards/{board_id}")
+async def update_board(board_id: int, updated_board: Board) -> None:
+    for i, board in enumerate(boards):
+        if board.id == board_id:
+            boards[i] = updated_board
+            return
+
+@app.delete("/boards/{board_id}")
+async def delete_boards(board_id: int) -> None:
+    for i, board in enumerate(boards):
+        if board.id == board_id:
+            boards.pop(i)
